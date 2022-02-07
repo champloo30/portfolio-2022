ca274bb987f3085baeb5b0b4e1fae6ed0b84b6b1
champloo30 <djld30@gmail.com>
2022-02-07 17:06:46 -0500

    next is responsiveness

--- code/src/components/menu/Menu.jsx
+++ code/src/components/menu/Menu.jsx
@@ -6,19 +6,19 @@ import iphone from "../../assets/icons8-iphone-x.svg";
 export default function Menu({ menuOpen, setMenuOpen, isDarkModeActive }) {
   return (
     <div className={isDarkModeActive ? "menu-dark " + (menuOpen && "active-dark") : "menu " + (menuOpen && "active")}>
-      <div className="about">
+      <div className="about-menu">
         <a href="#about" className="about-container" onClick={()=>setMenuOpen(false)}>
           <img src={person} alt="Person Icon" className="icon" />
           <h2>About Me</h2>
         </a>
       </div>
-      <div className="portfolio">
+      <div className="portfolio-menu">
         <a href="#portfolio" className="portfolio-container" onClick={()=>setMenuOpen(false)}>
           <img src={briefcase} alt="Briefcase Icon" className="icon" />
           <h2>Portfolio</h2>
         </a>
       </div>
-      <div className="contact">
+      <div className="contact-menu">
         <a href="#contact" className="contact-container" onClick={()=>setMenuOpen(false)}>
           <img src={iphone} alt="iPhone X Icon" className="icon" />
           <h2>Contact</h2>
